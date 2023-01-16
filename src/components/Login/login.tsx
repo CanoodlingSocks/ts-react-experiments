2f3c903a6bfa984547f83ad5841a257786da353f
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-01-16 07:37:02 +0100

    create login form

---
+++ src/components/Login/login.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import Container from "react-bootstrap/Container"
+const Login = () => {
+
+    const [validated, setValidated] = useState<boolean>(false);
+
+
+    
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    }
+
+    return(
+        <>
+        <div className="color-overlay d-flex justify-content-center align-items-center">
+        <Form className="rounded p-4 p-sm-3">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We use your data *evil laugh*
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check here to sell me your firstborn"></Form.Check>
+                <Form.Text className="text-muted">No takebacksies!</Form.Text>
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+        </div>
+        </>
+    );
+}
+
+export default Login;
